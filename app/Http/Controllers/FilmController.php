21563ec0d1cd194a5af99801c28f2505dc3dfbbb
Clansou <clement.souplet78@gmail.com>
2022-12-02 14:45:34 +0100

    Add next page previous page

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -8,18 +8,26 @@ class FilmController extends Controller
 {
     public function GetMovie ($page) {
     print_r($page);
+    $page_next = $page + 1;
+    $page_previous = $page -1;
+
     $api_url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page='.$page.'&api_key=c800206ebd27d3b6b6e7b19c646c4928';
     $json_data = file_get_contents($api_url);
     //print_r($json_data);
     $films_data = json_decode($json_data);
-    //print_r($films_data);
-    $films_data = $films_data->results;
-    //print_r($films_data);
-    foreach ($films_data as $film) {
+    $film_data = $films_data->results;
+    //print_r($film_data);
+    foreach ($film_data as $film) {
         echo "name: ".$film->title;
         echo "<br />";
         echo "description: ".$film->overview;
         echo "<br /> <br />";
+    }
+    if($page_next != 501){
+       echo "<a href=http://ustream.test/films/".$page_next."> next page";
+    }
+    if($page_previous != 0){
+        echo "<a href=http://ustream.test/films/".$page_previous."> previous page";
     }
 }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/{page}',[FilmController::class, 'GetMovie']);
+Route::get('/films/{page}',[FilmController::class, 'GetMovie']);
