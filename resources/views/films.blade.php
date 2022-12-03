6741c451a7f0849ef91d9a89ec900ecf76fd1f1f
Angel Hmeli <angel.hmeli@edu.devinci.fr>
2022-12-03 18:23:51 +0100

    FlexCard

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -25,7 +25,7 @@
 <h2 class="text-2xl font-bold mx-[4%] mt-[3%]">Top films</h2>
 <div class="grid grid-cols-4 justify-items-center">
     @foreach($films_data->results as $film)
-        <div class="filmCard m-4 text-lg shadow-2xl">
+        <div class="filmCard m-4 text-lg shadow-2xl flex flex-col w-[58%]">
             <h3 class="filmTitle font-bold">{{ $film->title }}</h3>
             <img src="https://image.tmdb.org/t/p/w220_and_h330_face/{{$film->poster_path }}" alt="Film Poster">
         </div>
