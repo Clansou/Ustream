572bd61490b4abaaacb5f68930437b0ca7895830
Clansou <clement.souplet78@gmail.com>
2022-12-06 15:00:33 +0100

    Update film page

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -79,7 +79,7 @@ class FilmController extends Controller
 
 
 
-        $api_url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&with_genres='.$genre_id.'&page='.$page.'&api_key=c800206ebd27d3b6b6e7b19c646c4928';
+        $api_url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.descx&with_genres='.$genre_id.'&page='.$page.'&api_key=c800206ebd27d3b6b6e7b19c646c4928';
         $json_data = file_get_contents($api_url);
         //print_r($json_data);
         $films_data = json_decode($json_data);
@@ -108,7 +108,7 @@ class FilmController extends Controller
 
     }
 
-    public function GetMovie(){
-        return view('film');
+    public function GetMovie($id_film){
+        return view('film',['id_film' => $id_film]);
     }
 }
