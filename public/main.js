5dcb7ce425a63af84e0f12420f393b367f71e7b9
Angel Hmeli <angel.hmeli@edu.devinci.fr>
2022-12-06 14:58:51 +0100

    Carrousel CAsi Done

--- public/main.js
+++ public/main.js
@@ -1,25 +1,28 @@
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-const carousel = document.querySelector(".carousel-container");
-const track = document.querySelector(".track");
-let width = carousel.offsetWidth;
-let index = 0;
+const prev = document.querySelector(".prev")
+const next = document.querySelector(".next")
+const carousel = document.querySelector(".carousel-container")
+const track = document.querySelector(".track")
+let width = carousel.offsetWidth
+let index = 0
 next.addEventListener("click", function (e) {
-  e.preventDefault();
-  index = index + 1;
-  prev.classList.add("show");
-  track.style.transform = "translateX(" + index * -width + "px)";
+  e.preventDefault()
+  index = index + 1
+  prev.style.display = "block"
+  track.style.transform = "translateX(" + index * -width + "px)"
   if (track.offsetWidth - index * width < index * width) {
-    next.classList.add("hide");
+    next.style.display = "none"
   }
 });
 prev.addEventListener("click", function (e) {
-  e.preventDefault();
-  index = index - 1;
-  next.classList.remove("hide");
-  track.style.transform = "translateX(" + index * -width + "px)";
+  e.preventDefault()
+  index = index - 1
+  next.style.display = "block"
+  track.style.transform = "translateX(" + index * -width + "px)"
   if (track.offsetWidth - index * width < index * width) {
-    next.classList.remove("hide");
+    prev.style.display = "none"
+  }
+  if (index === 0){
+    prev.style.display = "none"
   }
   
 });
