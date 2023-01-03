22a90bdd71f0c82bfed9913a37d141027c907ef0
Clansou <clement.souplet78@gmail.com>
2023-01-03 12:14:41 +0100

    :bug: Fix bug register

--- database/migrations/2023_01_03_103723_create_albums_table.php
+++ database/migrations/2023_01_03_103723_create_albums_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('albums', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('user_id');
+            $table->string('user_email');
             $table->integer('state');
             $table->boolean('modify');
             $table->timestamps();
--- resources/views/my_profil.blade.php
+++ resources/views/my_profil.blade.php
@@ -5,6 +5,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('signout') }}">Logout</a>
+    </li>
     <h1><?php echo Auth::user()->id?></h1>
     <h2><?php echo Auth::user()->name?></h2>
     <h3><?php echo Auth::user()->email?></h3>
