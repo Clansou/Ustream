d77c2294e1c2d0e61d9e7eab6fc88207e216b0bc
Clansou <clement.souplet78@gmail.com>
2023-01-04 14:14:23 +0100

    Add actor to film page

--- resources/views/film.blade.php
+++ resources/views/film.blade.php
@@ -68,14 +68,23 @@ session_start();
 @foreach($film->genres as $each_genre)
     <p class="">{{ $each_genre->name }}</p>
 @endforeach
+<?php
+$api_url = 'https://api.themoviedb.org/3/movie/'.$id_film.'/credits?api_key=c800206ebd27d3b6b6e7b19c646c4928&language=EN';
+$json_data = file_get_contents($api_url);
+$film = json_decode($json_data);
+//print_r($film);
+?>
+@foreach($film->cast as $character)
+    
+        <?php if($character->known_for_department == "Acting" && $character->profile_path != "" ){?>
+            <div class="flex my-8">
+                <p class="">Actor name : {{$character->name}}</p>
+                <img src="https://image.tmdb.org/t/p/w220_and_h330_face/{{$character->profile_path}}" alt="">
+                <p class="">character name : {{$character->character}}</p>
+        </div>
+    <?php }?>
+    
+@endforeach
 
-<script>
-    console.log({{$id_film}});
-    console.log("https://api.themoviedb.org/3/movie/{{$id_film}}?api_key=c800206ebd27d3b6b6e7b19c646c4928&language=EN");
-    axios.get("https://api.themoviedb.org/3/movie/{{$id_film}}?api_key=c800206ebd27d3b6b6e7b19c646c4928&language=EN").then(function (film_data) {
-    console.log(film_data.data);
-  })
-
-</script>
 </body>
 </html>
